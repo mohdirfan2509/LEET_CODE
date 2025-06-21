251960d9efa85097a4efaef56f39315bfae35e39
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2025-06-21 12:37:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (29.70%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,8 +3,7 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         unordered_set<int> uset(nums1.begin(),nums1.end());
         unordered_set<int> res;
-        int n1= nums1.size();
-        int n2= nums2.size();
+
 
         for(int x:nums2){
             if(uset.find(x) != uset.end()){
