4b40317811907ca5e47b45e5b445cc4afedadb73
MOHAMMAD IRFAN <mohammadirfan25092004@gmail.com>
2025-09-06 17:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.71%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -4,23 +4,22 @@ public:
         int p1=m-1,p2=n-1,p3=m+n-1;
         while(p1>=0 && p2>=0){
             if(nums1[p1]>nums2[p2]){
-                nums1[p3]=nums1[p1];
-                p1--;
-                p3--;
+               nums1[p3] =nums1[p1];
+               p3--;
+               p1--;
             }
             else{
                 nums1[p3]=nums2[p2];
-                p2--;
                 p3--;
+                p2--;
             }
         }
         while(p2>=0){
-            nums1[p3]=nums2[p2];
-            p3--;
-            p2--;
+                nums1[p3]=nums2[p2];
+                p3--;
+                p2--;
         }
         return ;
-    
         
     }
 };
