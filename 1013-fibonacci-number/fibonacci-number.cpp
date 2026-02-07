746aea65fac8a6fbe73d6cdc84f58400e422f15d
MOHAMMAD IRFAN <mohammadirfan25092004@gmail.com>
2026-02-07 23:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (35.95%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1) return n;
-
-        return fib(n-1)+ fib(n-2);
+        int dp[3];
+        dp[0]=0;
+        dp[1]=1;
         
+        for(int i=2;i<=n;i++){
+            dp[i%3]=dp[(i-1)%3]+dp[(i-2)%3];
+        }
+        return dp[n%3];
     }
 };
