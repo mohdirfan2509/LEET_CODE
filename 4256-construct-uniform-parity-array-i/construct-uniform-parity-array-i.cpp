387b320b8dfd1e78cdbfe6229d060e12f74be0ca
MOHAMMAD IRFAN <mohammadirfan25092004@gmail.com>
2026-09-02 10:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (67.80%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // int n = nums1.size();
+        // if (n <= 2)
+        //     return true;
+        // int odds = 0;
+        // int evens = 0;
+        // for (int x : nums1) {
+        //     if (x % 2 == 0) {
+        //         evens++;
+        //     } else {
+        //         odds++;
+        //     }
+        // }
+        // if (odds == 1) {
+        //     return false;
+        // } else {
+        //     return true;
+        // }
+        return true;
+    }
+};
